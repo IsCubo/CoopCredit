0d4574c1f8e628b9e6ccfb1c1f1225c938ccac4f
Shnaider Villafañe <shnaiderjordan04@gmail.com>
2025-12-09 13:35:35 -0500

    First commit from the proyect

---
+++ src/test/java/com/riwi/coopcredit/CoopCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.coopcredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopCreditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
